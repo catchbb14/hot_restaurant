d899ab9243506bf3e6447e16926c973441cdc388
catchbb14 <ericmorrow14@gmail.com>
2018-05-12 11:36:03 -0400

    First Commit

---
+++ app/data/table-data.js
@@ -0,0 +1,11 @@
+var tableArray = [
+	{
+		customerName: "Eric",
+		customerEmail: "eric@gmail.com",
+		customerID: "eric93",
+		phoneNumber: "919-292-9393",
+	}
+];
+
+// Note how we export the array. This makes it accessible to other files using require. 
+module.exports = tableArray; 
---
+++ app/routing/html-routes.js
@@ -0,0 +1,16 @@
+var path = require("path");
+
+module.exports = function(app) {
+    app.get('/tables', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/tables.html'));
+	});
+
+	app.get('/reserve', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/reserve.html'));
+	});
+
+	// If no matching route is found default to home
+	app.use(function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+}
